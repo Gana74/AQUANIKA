c45d1de8787d4e2baa04a33ab0bd0cd067505773
Dmitrii Appel <yamixam@gmail.com>
2025-07-07 14:55:05 +0500

    Build for deployment

--- vite.config.js
+++ vite.config.js
@@ -3,17 +3,18 @@ import { resolve } from "path";
 
 export default defineConfig({
   root: "src",
+  base: "/AQUANIKA/",
   build: {
     outDir: "../dist",
     emptyOutDir: true,
     rollupOptions: {
       input: {
         main: resolve(__dirname, "src/index.html"),
-        about: resolve(__dirname, "src/about/index.html"),
-        services: resolve(__dirname, "src/services/index.html"),
-        team: resolve(__dirname, "src/team/index.html"),
-        gallery: resolve(__dirname, "src/gallery/index.html"),
-        contacts: resolve(__dirname, "src/contacts/index.html"),
+        // about: resolve(__dirname, "src/about/index.html"),
+        // services: resolve(__dirname, "src/services/index.html"),
+        // team: resolve(__dirname, "src/team/index.html"),
+        // gallery: resolve(__dirname, "src/gallery/index.html"),
+        // contacts: resolve(__dirname, "src/contacts/index.html"),
       },
     },
   },
