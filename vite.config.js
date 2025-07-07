89186f20f6ffceae2bdc75bae176056e42aa5c67
Dmitrii Appel <yamixam@gmail.com>
2025-07-07 15:08:38 +0500

    Initial commit

--- vite.config.js
+++ vite.config.js
@@ -2,24 +2,19 @@ import { defineConfig } from "vite";
 import { resolve } from "path";
 
 export default defineConfig({
-  root: "src",
   base: "/AQUANIKA/",
   build: {
-    outDir: "../dist",
+    outDir: "dist",
     emptyOutDir: true,
+    sourcemap: true,
     rollupOptions: {
       input: {
         main: resolve(__dirname, "src/index.html"),
-        // about: resolve(__dirname, "src/about/index.html"),
-        // services: resolve(__dirname, "src/services/index.html"),
-        // team: resolve(__dirname, "src/team/index.html"),
-        // gallery: resolve(__dirname, "src/gallery/index.html"),
-        // contacts: resolve(__dirname, "src/contacts/index.html"),
       },
     },
   },
   server: {
-    port: 3000,
+    port: 5173,
     open: true,
   },
 });
