b3e57d16f77d332f546bbab9fc1fa194354e44a0
Dmitrii Appel <yamixam@gmail.com>
2025-08-24 21:27:03 +0500

    dd deploy workflow

--- vite.config.js
+++ vite.config.js
@@ -9,19 +9,24 @@ export default defineConfig(({ command }) => {
       outDir: "../docs",
       emptyOutDir: true,
       sourcemap: true,
-      // Настройка для корректного формирования путей
       assetsDir: "assets",
       cssCodeSplit: true,
       minify: true,
+      // Настройка копирования статических файлов
+      publicDir: "public",
       rollupOptions: {
         input: {
           main: resolve(__dirname, "src/index.html"),
+          home: resolve(__dirname, "src/pages/home.html"),
           about: resolve(__dirname, "src/pages/about.html"),
           team: resolve(__dirname, "src/pages/team.html"),
           reviews: resolve(__dirname, "src/pages/reviews.html"),
           vacancies: resolve(__dirname, "src/pages/vacancies.html"),
           gallery: resolve(__dirname, "src/pages/gallery.html"),
           spa: resolve(__dirname, "src/pages/spa-and-massage.html"),
+          header: resolve(__dirname, "src/components/partials/header.html"),
+          footer: resolve(__dirname, "src/components/partials/footer.html"),
+          sideMenu: resolve(__dirname, "src/components/partials/side-menu.html"),
         },
       },
     },
@@ -32,12 +37,16 @@ export default defineConfig(({ command }) => {
       historyApiFallback: {
         rewrites: [
           { from: /^\/$/, to: "/index.html" },
+          { from: /^\/home/, to: "/pages/home.html" },
           { from: /^\/about/, to: "/pages/about.html" },
           { from: /^\/team/, to: "/pages/team.html" },
           { from: /^\/reviews/, to: "/pages/reviews.html" },
           { from: /^\/vacancies/, to: "/pages/vacancies.html" },
           { from: /^\/gallery/, to: "/pages/gallery.html" },
           { from: /^\/services\/spa/, to: "/pages/spa-and-massage.html" },
+          { from: /^\/components\/header/, to: "/components/partials/header.html" },
+          { from: /^\/components\/footer/, to: "/components/partials/footer.html" },
+          { from: /^\/components\/side-menu/, to: "/components/partials/side-menu.html" },
         ],
       },
     },
